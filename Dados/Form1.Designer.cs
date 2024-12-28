7975b0ae0c485fc594255421a672fad617920dae
Gonzalo <gonzalorivero97@gmail.com>
2024-12-28 13:01:57 -0300

    Primera subida del proyecto

---
+++ Dados/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Dados
+{
+    partial class frmDados
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnGirar = new System.Windows.Forms.Button();
+            this.pctImagen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Presiona el boton para girar el dado";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(29, 133);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(113, 77);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGirar
+            // 
+            this.btnGirar.Location = new System.Drawing.Point(32, 225);
+            this.btnGirar.Name = "btnGirar";
+            this.btnGirar.Size = new System.Drawing.Size(110, 38);
+            this.btnGirar.TabIndex = 2;
+            this.btnGirar.Text = "Girar";
+            this.btnGirar.UseVisualStyleBackColor = true;
+            this.btnGirar.Click += new System.EventHandler(this.btnGirar_Click);
+            // 
+            // pctImagen
+            // 
+            this.pctImagen.Image = global::Dados.Properties.Resources._6;
+            this.pctImagen.Location = new System.Drawing.Point(294, 69);
+            this.pctImagen.Name = "pctImagen";
+            this.pctImagen.Size = new System.Drawing.Size(159, 194);
+            this.pctImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctImagen.TabIndex = 3;
+            this.pctImagen.TabStop = false;
+            // 
+            // frmDados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 302);
+            this.Controls.Add(this.pctImagen);
+            this.Controls.Add(this.btnGirar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDados";
+            this.Text = "Dados";
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnGirar;
+        private System.Windows.Forms.PictureBox pctImagen;
+    }
+}
+
---
+++ Dados/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dados
+{
+    public partial class frmDados : Form
+    {
+        public frmDados()
+        {
+            InitializeComponent();
+        }
+
+        Random R = new Random();  //Declara el objeto generador de numeros random, se declara afuera del procedimiento para que sea global
+        private void btnGirar_Click(object sender, EventArgs e)
+        {
+         Int32 n = R.Next(1 , 7);// el metodo next asigna un intervalo de valores, toma un valor mayor o igual al 1er numero y menor al ultimo.
+         lblResultado.Text = Convert.ToString(n);  
+            switch (n)
+            {
+             case 1:
+             pctImagen.Image = Dados.Properties.Resources._1;
+             break;
+             case 2:
+             pctImagen.Image = Dados.Properties.Resources._2;
+             break;
+             case 3:
+             pctImagen.Image = Dados.Properties.Resources._3;
+             break;
+             case 4:
+             pctImagen.Image = Dados.Properties.Resources._4;
+             break;
+             case 5:
+             pctImagen.Image = Dados.Properties.Resources._5;
+             break;
+             case 6:
+             pctImagen.Image = Dados.Properties.Resources._6;
+             break;
+             default:
+             break;
+            }
+        }
+    }
+}
